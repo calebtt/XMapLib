bbcaaf239208e711c42406ea2d8f005d0ae8d84c
calebtt <mounteriemachine@hotmail.com>
2021-12-18 05:40:58 -0600

    Fixed comment, fixed formatting.

--- XMapLib/StickMap.h
+++ XMapLib/StickMap.h
@@ -4,7 +4,6 @@ namespace sds
 {
 	/// <summary>
 	/// Used to denote which thumbstick is to be used for mouse movement.
-	///	This is not really a utility in the general sense, but for Xinmapper it is.
 	/// </summary>
 	enum class StickMap : int
 	{
