1c75ef3b429142fc70879c85ba154afaab03edfd
calebtt <mounteriemachine@hotmail.com>
2022-01-09 08:57:26 -0600

    Added custom concept constraint to templates in Arithmetic.h

--- XMapLib/Arithmetic.h
+++ XMapLib/Arithmetic.h
@@ -1,21 +1,24 @@
 #pragma once
 #include <cmath>
+#include <concepts>
 namespace sds::Utilities
 {
-	constexpr double ToDub(const auto something)
+	template<class T>
+	concept is_number_v = std::is_integral_v<T> || std::is_floating_point_v<T>;
+	constexpr double ToDub(const is_number_v auto something)
 	{
 		return static_cast<double>(something);
 	}
-	constexpr float ToFloat(const auto something)
+	constexpr float ToFloat(const is_number_v auto something)
 	{
 		return static_cast<float>(something);
 	}
-	constexpr int ToInt(const auto something)
+	constexpr int ToInt(const is_number_v auto something)
 	{
 		return static_cast<int>(something);
 	}
-	constexpr bool IsNormalF(const auto val)
+	constexpr bool IsNormalF(const is_number_v auto val)
 	{
 		return std::isnormal(static_cast<float>(val));
-	};
+	}
 }
