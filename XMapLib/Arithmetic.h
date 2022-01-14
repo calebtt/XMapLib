4f7c885dc4beca773e4e787d253e64c3377596e9
calebtt <mounteriemachine@hotmail.com>
2022-01-14 00:55:26 -0600

    marked arithmetic.h function templates noexcept

--- XMapLib/Arithmetic.h
+++ XMapLib/Arithmetic.h
@@ -4,19 +4,19 @@ namespace sds::Utilities
 {
 	template<class T>
 	concept is_number_v = std::is_integral_v<T> || std::is_floating_point_v<T>;
-	constexpr double ToDub(const is_number_v auto something)
+	constexpr double ToDub(const is_number_v auto something) noexcept
 	{
 		return static_cast<double>(something);
 	}
-	constexpr float ToFloat(const is_number_v auto something)
+	constexpr float ToFloat(const is_number_v auto something) noexcept
 	{
 		return static_cast<float>(something);
 	}
-	constexpr int ToInt(const is_number_v auto something)
+	constexpr int ToInt(const is_number_v auto something) noexcept
 	{
 		return static_cast<int>(something);
 	}
-	constexpr bool IsNormalF(const is_number_v auto val)
+	constexpr bool IsNormalF(const is_number_v auto val) noexcept
 	{
 		return std::isnormal(static_cast<float>(val));
 	}
