95fad8ba9c47334c1509b8e18d1daf36cbb50803
calebtt <mounteriemachine@hotmail.com>
2022-03-26 04:14:35 -0500

    refactored storage spec order

--- XMapLib/KeyboardSettings.h
+++ XMapLib/KeyboardSettings.h
@@ -7,13 +7,13 @@ namespace sds
 	struct KeyboardSettings
 	{
 		//Input Poller maximum number of XINPUT_KEYSTROKE structs to queue before dropping input.
-		constexpr static const int MAX_STATE_COUNT{ 128 };
+		static constexpr int MAX_STATE_COUNT{ 128 };
 		//Input Poller thread delay, in milliseconds.
-		constexpr static const int THREAD_DELAY_POLLER{ 10 };
+		static constexpr int THREAD_DELAY_POLLER{ 10 };
 		//Microseconds Delay Keyrepeat is the time delay a button has in between activations.
-		constexpr static const int MICROSECONDS_DELAY_KEYREPEAT{ 100000 };
+		static constexpr int MICROSECONDS_DELAY_KEYREPEAT{ 100000 };
 		//It is necessary to be able to distinguish these mapping values in KeyboardTranslator.
-		constexpr static std::array<int,8> THUMBSTICK_L_VK_LIST
+		static constexpr std::array<int,8> THUMBSTICK_L_VK_LIST
 		{
 			VK_PAD_LTHUMB_UP,
 			VK_PAD_LTHUMB_DOWN,
@@ -24,7 +24,7 @@ namespace sds
 			VK_PAD_LTHUMB_DOWNRIGHT,
 			VK_PAD_LTHUMB_DOWNLEFT
 		};
-		constexpr static std::array<int, 8> THUMBSTICK_R_VK_LIST
+		static constexpr std::array<int, 8> THUMBSTICK_R_VK_LIST
 		{
 			VK_PAD_RTHUMB_UP,
 			VK_PAD_RTHUMB_DOWN,
