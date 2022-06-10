45f669e79e2792b10e1a9b18c1a2ac4e626b9222
calebtt <calebktaylor@gmail.com>
2022-06-10 02:01:06 -0500

    added Smarts.h and a todo

---
+++ XMapLib/Smarts.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Contains helper functions and using declarations for constructing smart pointer types,
+// aliasing them here will make switching to a new (possibly better) smart pointer type in the future
+// much easier.
+
+//TODO
