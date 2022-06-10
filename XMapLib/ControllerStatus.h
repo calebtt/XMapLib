a6197f166595e8e23e06aa1bb78242fac2d6914d
calebtt <calebktaylor@gmail.com>
2022-06-10 01:45:14 -0500

    Altered variable names to be more descriptive.

--- XMapLib/ControllerStatus.h
+++ XMapLib/ControllerStatus.h
@@ -8,10 +8,10 @@ namespace sds
 	{
 		[[nodiscard]] static bool IsControllerConnected(const int pid) noexcept
 		{
-			XINPUT_KEYSTROKE ss{};
-			XINPUT_STATE xs{};
-			const auto xsRet = XInputGetState(pid, &xs);
-			const DWORD ret = XInputGetKeystroke(pid, 0, &ss);
+			XINPUT_KEYSTROKE keystrokeObj{};
+			XINPUT_STATE stateObj{};
+			const auto xsRet = XInputGetState(pid, &stateObj);
+			const DWORD ret = XInputGetKeystroke(pid, 0, &keystrokeObj);
 			return (ret == ERROR_SUCCESS || ret == ERROR_EMPTY) && (xsRet == ERROR_SUCCESS || xsRet == ERROR_EMPTY);
 		}
 	};
